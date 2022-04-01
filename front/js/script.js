93858f6bc66a5ed9f0affc78baae56795158540a
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2022-04-01 15:39:09 +0200

    Création d'une fonction fetchData

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,15 @@
 const apiURL = "http://localhost:3000/api/products";
+
+async function fetchData() {
+	try {
+		const response = await fetch(apiURL);
+		if (response.ok) {
+			const data = await response.json();
+			return displayProducts(data);
+		} else {
+			throw new Error("Échec du chargement de l'API");
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+}
