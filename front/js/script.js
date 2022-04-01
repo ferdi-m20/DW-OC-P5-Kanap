5cc79aef83dcd46b0acd3184f3e4b1f3da4c0ce3
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2022-04-01 15:41:29 +0200

    Création d'une fonction displayProducts

--- front/js/script.js
+++ front/js/script.js
@@ -13,3 +13,32 @@ async function fetchData() {
 		console.log(error.message);
 	}
 }
+
+function displayProducts(items) {
+	for (let item of items) {
+		// console.log(item);
+		let productLink = document.createElement("a");
+		productLink.setAttribute("href", `product.html?id=${item._id}`);
+		document.querySelector("#items").appendChild(productLink);
+
+		let productArticle = document.createElement("article");
+		productLink.appendChild(productArticle);
+
+		let productImg = document.createElement("img");
+		productImg.setAttribute("src", item.imageUrl);
+		productImg.setAttribute("alt", item.altTxt);
+		productArticle.appendChild(productImg);
+
+		let productTitle = document.createElement("h3");
+		productTitle.classList.add("productName");
+		productTitle.textContent = item.name;
+		productArticle.appendChild(productTitle);
+
+		let productDescription = document.createElement("p");
+		productDescription.classList.add("productDescription");
+		productDescription.textContent = item.description;
+		productArticle.appendChild(productDescription);
+	}
+}
+
+fetchData();
