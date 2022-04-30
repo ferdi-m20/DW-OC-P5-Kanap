081abb4ed887dc21b9dd5cd164f399c20859f09b
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2022-04-30 10:47:21 +0200

    Ajout de la fonction displaySelectedProduct

--- front/js/product.js
+++ front/js/product.js
@@ -17,3 +17,31 @@ async function getProduct() {
 		console.log(error.message);
 	}
 }
+
+function displaySelectedProduct(item) {
+	// console.log(item);
+	let productImg = document.createElement("img");
+	productImg.setAttribute("src", item.imageUrl);
+	productImg.setAttribute("alt", item.altTxt);
+	document.querySelector(".item__img").appendChild(productImg);
+
+	let productTitle = document.getElementById("title");
+	productTitle.textContent = item.name;
+
+	let productPrice = document.getElementById("price");
+	productPrice.textContent = item.price;
+
+	let productDescription = document.getElementById("description");
+	productDescription.textContent = item.description;
+
+	for (let color of item.colors) {
+		// console.log(color);
+		let productColors = document.createElement("option");
+		productColors.setAttribute("value", color);
+		productColors.textContent = color;
+		document.querySelector("#colors").appendChild(productColors);
+	}
+
+	const addButton = document.getElementById("addToCart");
+	addButton.addEventListener("click", checkCart);
+}
