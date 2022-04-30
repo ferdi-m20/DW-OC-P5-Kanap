b5d7571520df9adf67b7047f6e6c6fc5eb4c3daa
Ferdinand BLANCHART <ferdinand_m20@yahoo.fr>
2022-04-30 10:44:46 +0200

    Ajout de la fonction getProduct

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,19 @@
 let params = new URL(window.location.href).searchParams;
 // console.log(params);
 let productId = params.get("id");
+
+async function getProduct() {
+	try {
+		const response = await fetch(
+			"http://localhost:3000/api/products/" + productId
+		);
+		if (response.ok) {
+			const data = await response.json();
+			return displaySelectedProduct(data);
+		} else {
+			throw new Error("Failed to load API");
+		}
+	} catch (error) {
+		console.log(error.message);
+	}
+}
